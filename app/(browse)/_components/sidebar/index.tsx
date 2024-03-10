6a67d79457303348c4db768add4a304764281b2b
Jun <peng940825@gmail.com>
2024-03-10 22:30:58 +0800

    add recommended list

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,10 +1,26 @@
 import { Toggle } from "./toggle";
 import { Wrapper } from "./wrapper";
+import { Recommended, RecommendedSkeleton } from "./recommended";
 
-export function Sidebar() {
+import { getRecommended } from "@/lib/recommended-service";
+
+export async function Sidebar() {
+  const recommended = await getRecommended();
+
   return (
     <Wrapper>
       <Toggle />
+      <div className="space-y-4 pt-4 lg:pt-0">
+        <Recommended data={recommended} />
+      </div>
     </Wrapper>
+  );
+}
+
+export function SidebarSkeleton() {
+  return (
+    <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2d2e35] z-50">
+      <RecommendedSkeleton />
+    </aside>
   );
 }
